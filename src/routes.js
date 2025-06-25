f5e0d6113c745795c6bab5c06d58574d9e013569
Luis Facunde <luisfacunde19@gmail.com>
2025-06-25 14:37:51 -0300

    refactor: criação do arquivo de rotas, destinado todas as rotas no arquivo 'routes.js'.

---
+++ src/routes.js
@@ -0,0 +1,48 @@
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
+// UUDI => Unique Universal ID
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: "GET",
+    path: "/users",
+    handle: (req, res) => {
+      const users = database.select("users");
+
+      return res.end(JSON.stringify(users));
+    },
+  },
+  {
+    method: "POST",
+    path: "/users",
+    handle: (req, res) => {
+      const { name, email } = req.body;
+
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+      };
+
+      database.insert("users", user);
+
+      return res.writeHead(201).end();
+    },
+  },
+  {
+    method: "PUT",
+    path: "/users",
+    handle: (req, res) => {
+      return res.end("Atualiza informações do usuário");
+    },
+  },
+  {
+    method: "DELETE",
+    path: "/users",
+    handle: (req, res) => {
+      return res.end("Deleta um usuário");
+    },
+  },
+];
